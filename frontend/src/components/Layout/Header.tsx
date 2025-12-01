7aab1bd0442d0f9cb824f256c792b7064f268ff2
unknown <cmannunziato@gmail.com>
2025-12-01 14:02:45 -0500

    changed header red color slightly

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -13,9 +13,9 @@ const Header = () => {
                 <Link to="/" className="text-xl font-display">
                     <div className="text-xl font-display"><span className="mr-2">[</span>
                         <span className="text-text-primary dark:text-text-primary-dark">Z o n e </span>
-                        <span className="text-red-500">T y p e </span>
+                        <span className="text-[#ff0041]">T y p e </span>
                         <span className="inline-block  
-               p-[0.15rem] rotate-15 ml-[0.18rem] bg-red-500 ">R</span>
+               p-[0.15rem] rotate-15 ml-[0.18rem] bg-[#ff0041] ">R</span>
                         <span className="ml-2.5">]</span>
                     </div>
                 </Link>
@@ -23,25 +23,25 @@ const Header = () => {
                 <div className="flex justify-center items-center gap-6 font-header text-lg">
                     <div className="group cursor-pointer p-2 -m-2">
                         <span className="relative inline-block pb-1 -m-1">Race
-                        <span className="absolute h-1 w-0 bg-red-500 bottom-0 left-0 
+                        <span className="absolute h-1 w-0 bg-[#ff0041] bottom-0 left-0 
                         transition-all duration-200 group-hover:w-full skew-x-[-30deg]"></span>
                         </span>
                     </div>
                     <div className="group cursor-pointer p-2 -m-2">
                         <span className="relative inline-block pb-1 -m-1">Leaderboard
-                        <span className="absolute h-1 w-0 bg-red-500 bottom-0 left-0 
+                        <span className="absolute h-1 w-0 bg-[#ff0041] bottom-0 left-0 
                         transition-all duration-200 group-hover:w-full skew-x-[-30deg]"></span>
                         </span>
                     </div>
                     <div className="group cursor-pointer p-2 -m-2">
                         <span className="relative inline-block pb-1 -m-1">Profile
-                        <span className="absolute h-1 w-0 bg-red-500 bottom-0 left-0 
+                        <span className="absolute h-1 w-0 bg-[#ff0041] bottom-0 left-0 
                         transition-all duration-200 group-hover:w-full skew-x-[-30deg]"></span>
                         </span>
                     </div>
                     <div className="group cursor-pointer p-2 -m-2" onClick={toggleTheme}>
                         <span className="relative inline-block pb-1 -m-1">Theme
-                        <span className="absolute h-1 w-0 bg-red-500 bottom-0 left-0 
+                        <span className="absolute h-1 w-0 bg-[#ff0041] bottom-0 left-0 
                         transition-all duration-200 group-hover:w-full skew-x-[-30deg]"></span>
                         </span>
                     </div>
