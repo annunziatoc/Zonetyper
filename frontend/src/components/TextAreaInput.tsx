3e8beda78f04f08f4d4f058a45769979b807a969
unknown <cmannunziato@gmail.com>
2025-12-02 11:02:00 -0500

    added a limit of 627 chars for the str

--- frontend/src/components/TextAreaInput.tsx
+++ frontend/src/components/TextAreaInput.tsx
@@ -6,15 +6,15 @@ interface TextAreaInputProps {
 }
 
 const TextAreaInput = ({inputRef}: TextAreaInputProps) => {
-    const testStr = `On a Mars where ruthless corporate interests violently 
+    const rawStr = `On a Mars where ruthless corporate interests violently 
     collide with a homegrown independence movement as Earth-based overlords 
     battle for profits and power, Hakan Veil is an ex-professional enforcer 
-    equipped with military-grade body tech that's made him a human killing machine. 
-    But he's had enough of the turbulent red planet, and all he wants is a ticket 
-    back home-which is just what he's offered by the Earth Oversight organization, 
-    in exchange for being the bodyguard for an EO investigator. It's a beyond-easy 
-    gig for a heavy hitter like Veil … until it isn't.`
+    equipped with military-grade body 
+    `
         .trim().replace(/\s+/g, ' ').replace(/[–—]/g, '-');
+    
+    const testStr = rawStr.length > 627 ? rawStr.slice(0, 627) + '...' : rawStr;
+    
 
     const [textInput, setTextInput] = useState("")
     const [caretPos, setCaretPos] = useState({ left: 0, top: 0 })
