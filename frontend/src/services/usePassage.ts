2a460ca8bd213c47f1fe68faeca1614ed1170e8a
unknown <cmannunziato@gmail.com>
2025-12-09 14:33:52 -0500

    fetch random passage with validation added ssl bad need to find other quotes

---
+++ frontend/src/services/usePassage.ts
@@ -0,0 +1,45 @@
+import FetchRandomPassage from "./FetchRandomPassage.tsx";
+import {useEffect, useState} from 'react';
+
+// const UsePassage = () => {
+//
+//     const [passage, setPassage] = useState<{ content: string } | null>(null)
+//     const [loading, setLoading] = useState(false)
+//     const [error, setError] = useState<string | null>(null)
+//
+//     useEffect(() => {
+//         const loadPassage = async () => {
+//             try {
+//                 setLoading(true)
+//                 const data = await FetchRandomPassage();
+//                 setPassage(data)
+//             } catch (err) {
+//                 setError('Failed to load')
+//                 console.log(err)
+//             } finally {
+//                 setLoading(false)
+//             }
+//         };
+//         loadPassage().catch(err => console.log(err));
+//     }, []);
+//     return {passage, loading, error}
+// }
+//
+//
+// export default UsePassage;
+
+
+
+
+
+const usePassage = () => {
+    
+    
+}
+export
+
+
+
+
+
+
