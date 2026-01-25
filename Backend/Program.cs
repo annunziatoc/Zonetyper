407274ecb8609782886e38a795e42891b64d6d67
unknown <cmannunziato@gmail.com>
2026-01-24 23:15:52 -0500

    added a home route

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 var app = builder.Build();
+app.MapGet("/", () => "Zonetyper API is running!");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
