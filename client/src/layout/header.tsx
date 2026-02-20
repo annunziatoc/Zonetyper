62c400ef45c8a4293e42ec8f417f57f1b683a3dc
Chris Annunziato <cmannunziato@gmail.com>
2026-02-20 18:04:48 -0500

    added grid and nav bar

--- client/src/layout/header.tsx
+++ client/src/layout/header.tsx
@@ -3,8 +3,18 @@ import styles from './header.module.css'
 const Header = () => {
     return (
         <header className={styles.header}>
-            <nav>
-                header
+            <nav className={styles.nav}>
+                <div className={styles.logo}>
+                    <span className={styles['logo-zone']}>[ Z o n e </span>
+                    <span className={styles['logo-typer']}>T y p e r ]</span>
+                    
+                </div>
+                <div className={styles['nav-items']}>
+                    <span>Race</span>
+                    <span>Leaderboard</span>
+                    <span>Profile</span>
+                    <span>Theme</span>
+                </div>
             </nav>
         </header>
     )
--- client/src/layout/layout.tsx
+++ client/src/layout/layout.tsx
@@ -1,14 +1,16 @@
 import Header from "./header";
 import Footer from "./footer";
 import { Outlet } from "react-router";
+import styles from './layout.module.css'
+
 
 const Layout = () => {
     return (
-        <>
+        <div className={styles.layout}>
             <Header />
             <Outlet />
             <Footer />
-        </>
+        </div>
     )
 }
 
