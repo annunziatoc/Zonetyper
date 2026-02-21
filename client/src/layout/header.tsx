2f15fb91f8c1b5d022da33984ec50e0c89b1e6aa
Chris Annunziato <cmannunziato@gmail.com>
2026-02-21 14:22:59 -0500

    added shadowing around footer buttons

--- client/src/layout/header.tsx
+++ client/src/layout/header.tsx
@@ -4,17 +4,22 @@ import styles from './header.module.css'
 const Header = () => {
 
     const [theme, setTheme] = useState<'light' | 'dark'>('dark')
+    const [animate, setAnimate] = useState(false)
 
 
     useEffect(() => {
         document.documentElement.style.colorScheme = theme
     }, [theme])
 
+    const onClickLogo = () => {
+        setAnimate(!animate)
+    }
+
 
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
-                <div className={styles.logo}>
+                <div className={`${styles.logo} ${animate ? styles['logo-animate'] : ''}`} onClick={() => onClickLogo()}>
                     <span className={styles['logo-zone']}>[ Z o n e </span>
                     <span className={styles['logo-typer']}>T y p e r ]</span>
                 </div>
@@ -22,7 +27,7 @@ const Header = () => {
                     <span>Race</span>
                     <span>Leaderboard</span>
                     <span>Profile</span>
-                    <span onClick={() => setTheme(t => t === 'light' ? 'dark' : 'light' )} className={styles['theme-toggle']}>Theme</span>
+                    <span onClick={() => setTheme(t => t === 'light' ? 'dark' : 'light')}>Theme</span>
                 </div>
             </nav>
         </header>
