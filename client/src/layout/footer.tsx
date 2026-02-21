9ad1315b13651842fdca81fa147dbf1a0fc0da9a
Chris Annunziato <cmannunziato@gmail.com>
2026-02-20 20:18:45 -0500

    footer added

--- client/src/layout/footer.tsx
+++ client/src/layout/footer.tsx
@@ -3,7 +3,20 @@ import styles from './footer.module.css'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            footer
+
+
+            <div className={styles.guide}>
+                <div className={styles.tab}>Tab</div>
+                <div>To Restart</div>
+            </div>
+
+                <div className={styles.guide}>
+                <div className={styles.click}>Click</div>
+                <div>Screen To Gain Focus</div>
+            </div>
+
+
+            
         </footer>
 
     )
--- client/src/layout/header.tsx
+++ client/src/layout/header.tsx
@@ -1,19 +1,28 @@
+import { useEffect, useState } from "react";
 import styles from './header.module.css'
 
 const Header = () => {
+
+    const [theme, setTheme] = useState<'light' | 'dark'>('dark')
+
+
+    useEffect(() => {
+        document.documentElement.style.colorScheme = theme
+    }, [theme])
+
+
     return (
         <header className={styles.header}>
             <nav className={styles.nav}>
                 <div className={styles.logo}>
                     <span className={styles['logo-zone']}>[ Z o n e </span>
                     <span className={styles['logo-typer']}>T y p e r ]</span>
-                    
                 </div>
                 <div className={styles['nav-items']}>
                     <span>Race</span>
                     <span>Leaderboard</span>
                     <span>Profile</span>
-                    <span>Theme</span>
+                    <span onClick={() => setTheme(t => t === 'light' ? 'dark' : 'light' )} className={styles['theme-toggle']}>Theme</span>
                 </div>
             </nav>
         </header>
