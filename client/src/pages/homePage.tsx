b9881f0417c8dad1f47c6b96440b298ac166f95a
Chris Annunziato <cmannunziato@gmail.com>
2026-02-27 13:40:25 -0500

    added home page logic with text tracking

--- client/src/pages/homePage.tsx
+++ client/src/pages/homePage.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import styles from './homePage.module.css'
 
 const HomePage = () => {
@@ -9,6 +9,8 @@ const HomePage = () => {
         id: string;
     }
 
+    const caretRef = useRef<HTMLSpanElement>(null);
+    const [cursorPos, setCursorPos] = useState({top: 0, left: 0, height: 0, width: 0})
     const [textOnScreen] = useState(`"Culture is like a smog. To live within it,
          you must breathe some of it in and, inevitably, be contaminated."`)
     const [currIdx, setCurrIdx] = useState(0);
@@ -20,28 +22,67 @@ const HomePage = () => {
         }))
     )
 
+
+    useEffect(() => {
+        const rect = caretRef.current?.getBoundingClientRect()
+        if (!rect) return
+        setCursorPos({ top: rect.top, left: rect.left, height: rect.height, width: rect.width })
+    },[currIdx])
+
     return (
         <main className={styles.main}>
             <div className={styles.typingMask}>
                 <div onKeyDown={(ev) => {
-                    if (ev.key === charsArr[currIdx].char) {
-                        setCharsArr((prev) => prev.map((cs, i) => {
-                            if (i === currIdx) {
-                                return { ...cs, status: true }
+
+                    switch (ev.key) {
+                        //if chars match
+                        case 'Backspace':
+                            {
+                                if (currIdx > 0) {
+                                    setCharsArr((prev) => prev.map((cs, i) => {
+                                        //if index match
+                                        return i === currIdx - 1 ? { ...cs, status: null } : cs
+                                    }))
+
+                                    setCurrIdx((curr) => curr - 1)
+                                }
+                                break;
                             }
-                            return cs
-                        }))
+                        case 'Enter': {
+                            setCharsArr((prev) => prev.map((cs, i) => {
+                                return i === currIdx ? { ...cs, status: null } : cs
+                            }))
+                            setCurrIdx((curr) => curr + 1)
+                            break;
+                        }
+
+                        case charsArr[currIdx].char: {
+                            setCharsArr((prev) => prev.map((cs, i) => {
+                                return i === currIdx ? { ...cs, status: true } : cs
+                            }))
+                            setCurrIdx((curr) => curr + 1)
+
+                            break;
+                        }
+                        default: {
+                            if (ev.key.length !== 1) break
+                            setCharsArr((prev) => prev.map((cs, i) => {
+                                return i === currIdx ? { ...cs, status: false } : cs
+                            }))
+                            setCurrIdx((curr) => curr + 1)
+                            break;
+                        }
                     }
-                    setCurrIdx((curr) => curr + 1)
                 }}
                     className={styles.surface}
                     tabIndex={0}>
-                    {charsArr.map((cs) => (
-                        <span key={cs.id}>{cs.char}</span>
+                    {charsArr.map((cs, i) => (
+                        <span key={cs.id} className={cs.status === true ? styles.correct : cs.status === false ? styles.incorrect : ''} ref={i === currIdx ? caretRef : null}>{cs.char}</span>
                     ))}
+                    <div className={styles.caret} style={{position: 'fixed', top: cursorPos.top, left: cursorPos.left, height: cursorPos.height, width: cursorPos.width}}/>
                 </div>
             </div>
-        </main>
+        </main >
     )
 
 }
