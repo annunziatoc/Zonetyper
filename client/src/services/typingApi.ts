56cbf4fa2f76a6483f3f83861c581cf0ab6388ff
Chris Annunziato <cmannunziato@gmail.com>
2026-03-06 17:20:39 -0500

    updated quotes api to api-ninja

--- client/src/services/typingApi.ts
+++ client/src/services/typingApi.ts
@@ -2,19 +2,21 @@
     export const getNewText = async (): Promise<string> => {
 
         try {
-            const response = await fetch("https://api.quotable.io/random")
+            const response = await fetch("https://api.api-ninjas.com/v2/randomquotes", {
+                headers: {'X-Api-Key': import.meta.env.VITE_API_NINJA_KEY}
+            })
 
             if (!response.ok) {
                 throw new Error(`Response status: ${response.status}`)
             }
             const result = await response.json();
         
-            return result.content
+            return result[0].quote
         }
 
         catch (error) {
             console.error((error));
-            return 'Culture is like a smog. To live within it, you must breathe some of it in and, inevitably, be contaminated.';
+            return 'There is an issue with the api call! Working on it...';
         }
     }
 
