a311f0b761482df1d01473492a8b62053602dc0a
Chris Annunziato <cmannunziato@gmail.com>
2026-03-27 11:25:57 -0400

    removed wpm from mainpage

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -2,12 +2,9 @@ import { useEffect, useRef } from "react";
 import TypingSurface from "../typing/TypingSurface";
 import styles from './MainPage.module.css'
 import Timer from '../typing/components/Timer'
-import WPM from "../typing/components/WPM";
-import useTypingStore from "../typing/hooks/useTypingStore";
 
 const MainPage = () => {
 
-    const { endTime } = useTypingStore();
     const surfaceRef = useRef<HTMLDivElement>(null)
     useEffect(() => {
         surfaceRef.current?.focus()
@@ -20,7 +17,6 @@ const MainPage = () => {
             </div>
             <div className={styles.typingSpeedWrapper}>
                 <Timer />
-                {/* {endTime > 0 && <WPM />} */}
             </div>
             <TypingSurface surfaceRef={surfaceRef} />
         </div>
