b18dbb45541fb0d13899770f83263bdc99306c27
Chris Annunziato <cmannunziato@gmail.com>
2026-03-27 15:53:10 -0400

    feat-remove timer on complete

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -2,9 +2,11 @@ import { useEffect, useRef } from "react";
 import TypingSurface from "../typing/TypingSurface";
 import styles from './MainPage.module.css'
 import Timer from '../typing/components/Timer'
+import useTypingStore from "../typing/hooks/useTypingStore";
 
 const MainPage = () => {
 
+    const { endTime } = useTypingStore()
     const surfaceRef = useRef<HTMLDivElement>(null)
     useEffect(() => {
         surfaceRef.current?.focus()
@@ -16,7 +18,9 @@ const MainPage = () => {
             <div className={styles.typingSpeedWrapper}>
             </div>
             <div className={styles.typingSpeedWrapper}>
-                <Timer />
+                <div style={{ visibility: !endTime ? 'visible' : 'hidden' }}>
+                    <Timer />
+                </div>
             </div>
             <TypingSurface surfaceRef={surfaceRef} />
         </div>
