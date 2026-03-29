2264f9e75b0074417e3e25ba947f20551a68d915
Chris Annunziato <cmannunziato@gmail.com>
2026-03-29 11:43:41 -0400

    fix: refocus typing surface after results screen transition with delay > framer remount

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -9,11 +9,15 @@ const MainPage = () => {
 
     const { endTime } = useTypingStore()
     const surfaceRef = useRef<HTMLDivElement>(null)
-     useEffect(() => {
-        if (!endTime) surfaceRef.current?.focus()
+    useEffect(() => {
+        if (!endTime) {
+            setTimeout(() => {
+                surfaceRef.current?.focus()
+            },225)
+        }
     }, [endTime])
     useTabReset()
-   
+
 
     return (
         <div className={styles.mainPage} onClick={() => surfaceRef.current?.focus()}>
@@ -21,7 +25,7 @@ const MainPage = () => {
             </div>
             <div className={styles.typingSpeedWrapper}>
                 <div style={{ visibility: !endTime ? 'visible' : 'hidden' }}>
-                    <Timer/>
+                    <Timer />
                 </div>
             </div>
             <TypingSurface surfaceRef={surfaceRef} />
