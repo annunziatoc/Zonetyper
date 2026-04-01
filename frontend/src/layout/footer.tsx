982ba161a24de6503cc68c4185b4ae91e3891758
Chris Annunziato <cmannunziato@gmail.com>
2026-04-01 14:03:18 -0400

    feat-show loading state while user stats fetch resolves, updated colors in general

--- frontend/src/layout/footer.tsx
+++ frontend/src/layout/footer.tsx
@@ -3,15 +3,15 @@ import styles from './footer.module.css'
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-
-
-            <div className={styles.guide}>
-                <div className={styles.tab}>Tab</div>
-                <div>For New Quote</div>
-            </div>
-            <div className={styles.guide}>
-                <div className={styles.click}>Click</div>
-                <div>Screen To Gain Focus</div>
+            <div className={styles.footerInner}>
+                <div className={styles.guide}>
+                    <div className={styles.tab}>Tab</div>
+                    <div>For New Quote</div>
+                </div>
+                <div className={styles.guide}>
+                    <div className={styles.click}>Click</div>
+                    <div>Screen To Gain Focus</div>
+                </div>
             </div>
 
         </footer>
--- frontend/src/typing/components/ResultsScreen.tsx
+++ frontend/src/typing/components/ResultsScreen.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import useTypingStore from "../hooks/useTypingStore";
 import { getUserStats, type userStatsDto } from "../services/userStatsService";
 import styles from './ResultsScreen.module.css'
+import Loading from "../../components/Loading";
 
 const ResultsScreen = () => {
 
@@ -19,27 +20,26 @@ const ResultsScreen = () => {
     return (
         <div className={styles.container}>
             <div className={styles.heading}>Session Results</div>
-            <div className={styles.card}>
-                <ul className={styles.statsGrid}>
-                    <li>Speed: <span>{`${finalWpm} `}<small>wpm</small></span></li>
-                    <li>Accuracy: <span>{`${finalAcc} %`}</span></li>
-                    {/* <li>Duration: <span>{`${finalDur} s`}</span></li>
+                <div className={styles.card}>
+                    <ul className={styles.statsGrid}>
+                        <li>Speed: <span>{`${finalWpm} `}<small>wpm</small></span></li>
+                        <li>Accuracy: <span>{`${finalAcc} %`}</span></li>
+                        {/* <li>Duration: <span>{`${finalDur} s`}</span></li>
                     <li>Errors: <span>{errorCount}</span></li> */}
-                    <li>Top Speed: <span>{`${stats?.topSpeed} `}<small>wpm</small></span></li>
-                    <li>Last 5 Avg: <span>
-                        {(() => {
-                            if (!stats?.recentStats) return ''
-                            let sum = 0;
-                            for (const s of stats.recentStats) sum += s.wpm
-                            return Math.round(sum / stats.recentStats.length)
-                        })()}
-                        <small> wpm</small>
-                    </span></li>
-                </ul>
-
-                <div className={styles.graph}>
+                        {stats ?  <li>Top Speed: <span>{`${stats?.topSpeed} `}<small>wpm</small></span></li> : <Loading/>}
+                       {stats ? <li>Last 5 Avg: <span>
+                            {(() => {
+                                if (!stats?.recentStats) return ''
+                                let sum = 0;
+                                for (const s of stats.recentStats) sum += s.wpm
+                                return Math.round(sum / stats.recentStats.length)
+                            })()}
+                            <small> wpm</small>
+                        </span></li> : <Loading/> }
+                    </ul>
+                    <div className={styles.graph}>
+                    </div>
                 </div>
-            </div>
             {/* <div className={styles.nextSession}>Tab for Next Quote</div> */}
         </div>
     )
